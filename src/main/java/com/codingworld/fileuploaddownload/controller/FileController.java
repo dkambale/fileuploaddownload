28cfaadf7cb5e786157f28b7a53022583cfac184
dadakambale18@gmail.com <dadakambale18@gmail.com>
2023-01-08 17:41:41 +0530

    adding postgres and local file

--- src/main/java/com/codingworld/fileuploaddownload/controller/FileController.java
+++ src/main/java/com/codingworld/fileuploaddownload/controller/FileController.java
@@ -1,11 +1,5 @@
 package com.codingworld.fileuploaddownload.controller;
 
-import com.codingworld.fileuploaddownload.service.FileService;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -18,47 +12,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.codingworld.fileuploaddownload.service.FileService;
+
 @RestController
 public class FileController {
 
-  @Autowired
-  FileService fileService;
+	@Autowired
+	FileService fileService;
 
-  @PostMapping("/upload")
-  public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile) {
-    try {
-      fileService.uploadFile(uploadfile);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
+	@PostMapping("/upload")
+	public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile) {
+		try {
+			fileService.uploadFile(uploadfile);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 
-    return new ResponseEntity<>(HttpStatus.OK);
-  }
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-  @GetMapping("/download")
-  public ResponseEntity<Resource> downloadFile(@RequestParam("filename") String filename) {
-    // Load file as Resource
-    Resource resource = null;
-    try {
-      resource = fileService.downloadFile(filename);
+	@GetMapping("/download")
+	public ResponseEntity<Resource> downloadFile(@RequestParam("filename") String filename) {
 
-      // Try to determine file's content type
-      String contentType = null;
+		Resource resource = null;
+		try {
+			resource = fileService.downloadFile(filename);
+			String contentType = "application/octet-stream";
 
-      // Fallback to the default content type if type could not be determined
-      if (contentType == null) {
-        contentType = "application/octet-stream";
-      }
+			return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+					.body(resource);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 
-      return ResponseEntity.ok()
-          .contentType(MediaType.parseMediaType(contentType))
-          .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-          .body(resource);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-  }
+	}
 }
